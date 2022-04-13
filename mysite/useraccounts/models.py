c9cbc8944b7d4133c52cba0070384faa073e36c4
katelynv <katelynvi@gmail.com>
2022-04-13 12:22:52 -0700

    Fix errors

--- mysite/useraccounts/models.py
+++ mysite/useraccounts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.model):
+class User(models.Model):
     userID = models.AutoField(primary_key=True)
     username = models.CharField(max_length = 200)
     userpass = models.CharField(max_length = 100) 
