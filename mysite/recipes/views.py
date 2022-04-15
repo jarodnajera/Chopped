af3a24ee719c4a1b9a15d69d42124376eaf4d966
Raymond M Booth <rmbooth92084@gmail.com>
2022-04-15 11:57:54 -0700

    enabled some specific search
    gave functionality to the search that you can search by name, minutes, ingredients, or even something in the description of the recipe. Just got to put the correct characters in the beginning. n- for name, m- for minutes, d- for description, and i- for ingredients

--- mysite/recipes/views.py
+++ mysite/recipes/views.py
@@ -6,7 +6,21 @@ from django.shortcuts import render
 def search_recipes(request):
     if request.method == "POST":
         searched = request.POST['searched'] #obtain value from 'searched' variable in html page
-        recipes =  Recipe.objects.filter(name__contains=searched ) #find recipes that contains 'searched' in their name and assign to variable recipes
+        #print(searched[:1].strip() == "n")
+        if searched[:2].strip() == "n-": #search by name
+            searched = searched[2:] #This will enable searching in different fields
+            recipes = Recipe.objects.filter(name__contains=searched ) #find recipes that contains 'searched' in their name and assign to variable recipes
+        elif searched[:2].strip() == "m-": #search by minutes
+            searched = searched[2:] #This will enable searching in different fields
+            recipes = Recipe.objects.filter(minutes__contains=searched ) #find recipes that contains 'searched' in their name and assign to variable recipes
+        elif searched[:2].strip() == "d-": #search by description
+            searched = searched[2:] #This will enable searching in different fields
+            recipes = Recipe.objects.filter(description__contains=searched ) #find recipes that contains 'searched' in their name and assign to variable recipes
+        elif searched[:2].strip() == "i-": #search by ingredients
+            searched = searched[2:] #This will enable searching in different fields
+            recipes = Recipe.objects.filter(ingredients__contains=searched ) #find recipes that contains 'searched' in their name and assign to variable recipes
+        else: #default search by name
+            recipes = Recipe.objects.filter(name__contains=searched ) #find recipes that contains 'searched' in their name and assign to variable recipes
         return render(request, 'recipes/search_recipe.html', {'searched':searched, 'recipes':recipes}) 
     else:
         return render(request, 'recipes/search_recipe.html', {})
