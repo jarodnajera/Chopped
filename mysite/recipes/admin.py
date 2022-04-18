e182baa3956d00f162562080f2c3f2a94d8db4bf
joeypercia <jperc004@ucr.edu>
2022-04-18 14:17:54 -0700

    remove django-import-export library

--- mysite/recipes/admin.py
+++ mysite/recipes/admin.py
@@ -1,19 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from import_export import resources
+#from import_export import resources
 from .models import Recipe, Ingredient
-
 
 admin.site.register(Recipe)
 admin.site.register(Ingredient)
-
-class RecipeResource(resources.ModelResource):
-
-    class Meta:
-        model = Recipe   
-
-class IngredientResource(resources.ModelResource):
-
-    class Meta:
-        model = Ingredient 
