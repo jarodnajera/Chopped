3a080fbd94554847e8a047c3bbab37934cf2e252
joeypercia <jperc004@ucr.edu>
2022-04-24 22:23:30 -0700

    add script to store data in-memory
    import_data.py reads a .json file and stores its data in-memory.

--- mysite/recipes/apps.py
+++ mysite/recipes/apps.py
@@ -1,6 +1,14 @@
 from django.apps import AppConfig
-
+from import_data import LoadJSON
 
 class RecipesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'recipes'
+    imported_recipes = LoadJSON("./recipes.json")
+
+    def ready(self):
+        pass
+        #startup code 
+        #file_path = os.path.expanduser('./recipes.json')
+        
+        #db = json.load(open(self.location , "r"))
