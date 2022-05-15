14f959a2546662c17bece41ef1bff13546276a89
unknown <noahstodder15@gmail.com>
2022-05-15 14:49:33 -0700

    added upvote feature

--- chopped-website/src/views/PopularRecipesView.vue
+++ chopped-website/src/views/PopularRecipesView.vue
@@ -3,6 +3,11 @@ export default {
   data() {
     return {
       pop_recipes: [],
+      count1: 0,
+      count2: 0,
+      count3: 0,
+      count4: 0,
+      count5: 0
     };
   },
   methods: {
@@ -28,7 +33,7 @@ export default {
           this.pop_recipes[i].rating +
           "</p>";
       });
-    },
+    }
   },
   mounted() {
     this.get_popular_recipes();
@@ -39,11 +44,56 @@ export default {
 <template>
   <div onload="get_popular_recipes" class="popular-recipes">
     <h1 id="title">Popular Recipes</h1>
-    <div class="pop-recipe" id="popular-recipe-1"></div>
-    <div class="pop-recipe" id="popular-recipe-2"></div>
+    <div class="pop-recipe-container">
+      <div class="pop-recipe" id="popular-recipe-1"></div>
+      <div class = "upvote">
+        <br><br>
+        <button type = "button" @click = "count1++"> ▲ </button>
+        <p className = "votecounter">{{count1}}</p>
+        <button  type = "button" @click = "count1--"> ▼ </button>
+        <p></p>
+      </div>
+    </div>
+    <div class="pop-recipe-container">
+      <div class="pop-recipe" id="popular-recipe-2"></div>
+      <div class = "upvote">
+        <br><br>
+        <button type = "button" @click = "count2++"> ▲ </button>
+        <p className = "votecounter">{{count2}}</p>
+        <button  type = "button" @click = "count2--"> ▼ </button>
+      </div>
+    </div>
+    <div class="pop-recipe-container">
+      <div class="pop-recipe" id="popular-recipe-3"></div>
+      <div class = "upvote">
+        <br><br>
+        <button type = "button" @click = "count3++"> ▲ </button>
+        <p className = "votecounter">{{count3}}</p>
+        <button  type = "button" @click = "count3--"> ▼ </button>
+      </div>
+    </div>
+    <div class="pop-recipe-container">
+      <div class="pop-recipe" id="popular-recipe-4"></div>
+      <div class = "upvote">
+        <br><br>
+        <button type = "button" @click = "count4++"> ▲ </button>
+        <p className = "votecounter">{{count4}}</p>
+        <button  type = "button" @click = "count4--"> ▼ </button>
+      </div>
+    </div>
+    <div class="pop-recipe-container">
+      <div class="pop-recipe" id="popular-recipe-5"></div>
+      <div class = "upvote">
+        <br><br>
+        <button type = "button" @click = "count5++"> ▲ </button>
+        <p className = "votecounter">{{count5}}</p>
+        <button  type = "button" @click = "count5--"> ▼ </button>
+      </div>
+    </div>
+    <!-- <div class="pop-recipe" id="popular-recipe-2"></div>
     <div class="pop-recipe" id="popular-recipe-3"></div>
     <div class="pop-recipe" id="popular-recipe-4"></div>
-    <div class="pop-recipe" id="popular-recipe-5"></div>
+    <div class="pop-recipe" id="popular-recipe-5"></div> -->
   </div>
 </template>
 
@@ -68,5 +118,19 @@ export default {
   padding: 60px;
   font-family: "Chau Philomene One", sans-serif;
   color: white;
+  min-width: 1000px;
+}
+
+.upvote{
+  text-align: center;
+  align-content: center;
+  vertical-align: middle;
+  justify-content: center;
+  max-height: 100px;
+}
+
+.pop-recipe-container{
+  display: flex;
+  flex-direction: row;
 }
 </style>
